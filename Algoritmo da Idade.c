7c08e0a6e49b6dddcf5488dcd4b12bf1e96ceedf
João Vitor Guimarães dos Santos <70954802+jvguima04@users.noreply.github.com>
2023-04-07 10:34:31 -0300

    Add files via upload

---
+++ Algoritmo da Idade.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+	
+	int ano_nascimento = 2004;
+	int ano_atual = 2023;
+	
+	int resultado = (ano_atual-ano_nascimento);
+	
+	printf(" Joao tem %d anos",resultado);
+	
+}
